b4f6b3b40a4617d9a1434b011869ee2302c23d59
Nicholas Bruce <nicholas.bruce@nrc-cnrc.gc.ca>
2026-09-15 14:33:42 +0200

    feat(all): add zellij config and rename files to be clearer

--- src/hx-and-friends/install.sh
+++ src/hx-and-friends/install.sh
@@ -87,9 +87,10 @@ USER_HOME=$(getent passwd "$USERNAME" | cut -d: -f6)
 
 # # copy configs to user home if different
 # if [ "$USERNAME" != "root" ]; then
-mkdir --parents "$USER_HOME/.config/helix" "$USER_HOME/.config/dprint"
-cp config.toml "$USER_HOME/.config/helix/config.toml"
-cp languages.toml "$USER_HOME/.config/helix/languages.toml"
+mkdir --parents "$USER_HOME/.config/helix" "$USER_HOME/.config/dprint" "$USER_HOME/.config/zellij"
+cp config.kdl "$USER_HOME.config/zellij/config.kdl"
+cp helix_config.toml "$USER_HOME/.config/helix/config.toml"
+cp helix_languages.toml "$USER_HOME/.config/helix/languages.toml"
 cp dprint.json "$USER_HOME/.config/dprint/dprint.json"
 cp gitconfig "$USER_HOME/.gitconfig"
 cp starship.toml "$USER_HOME/starship.toml"
